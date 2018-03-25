98cdcf103152c0760c486205c53408856ca530cd
Jason Guo <jasonyzguo@gmail.com>
2018-03-25 15:02:55 -0400

    Refactor code

--- grid.js
+++ grid.js
@@ -33,28 +33,6 @@ function grid(array, size) {
   }
 }
 
-
-function placeTab() {
-
-}
-
-function blockInteract(arr) {
-  var x = mouseX + viewX;
-  var y = mouseY + viewY;
-
-  if (mouseIsPressed) {
-    if (mouseButton == LEFT) {
-      if (floor(x / gridSize) < arrWidth && floor(x / gridSize) >= 0 && floor(y / gridSize) < arrHeight && floor(y / gridSize) >= 0) {
-        //changes the object once it clicks
-        //arr[floor(x / gridSize)][floor(y / gridSize)] = forest;
-      }
-    }
-  }
-
-}
-
-
-
 function mouseWheel(event) {
   if (event.delta > 0) {
     gridSize += zoomSpeed;
